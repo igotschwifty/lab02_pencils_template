8d1b5ea163fcd19f2b07f7f0381802aa37471613
igotschwifty <84202966+igotschwifty@users.noreply.github.com>
2022-09-14 18:39:06 -0400

    Add files via upload

--- src/ui/colorcanvas.cpp
+++ src/ui/colorcanvas.cpp
@@ -5,38 +5,65 @@ void ColorCanvas::initializeColorCanvas() {
 
     // Task 6: initialize a 10 x 10 colorful canvas where each pixel has an
     //         RGBA value of (0, 123, 123, 255)
+    std::vector<RGBA> values(100);
+    RGBA value = RGBA{0, 123, 123, 255};
+    std::fill(values.begin(), values.end(), value);
+    m_canvasData = values;
 
     // Task 8: call the drawFlower() function to display the canvas with
     //         a couple of flowers
+    drawFlower(3,3);
+    drawFlower(0,0);
+    drawFlower(9,3);
+    drawFlower(9,9);
 }
 
 int ColorCanvas::posToIndex(int x, int y) {
     // Task 7: convert an (x, y) position into an index you
     //         can use to index into the array of canvas data
-
-    return 0;
+    return y*10 + x;
 }
 
 void ColorCanvas::drawFlower(int x, int y) {
     // Task 8: draw flowers on your canvas. The inputs of this function are
     //         the x and y coordinates of the center of the flower
+    m_canvasData[posToIndex(x,y)] = RGBA{255,255,255,255};
+    if (x < 9) {
+        m_canvasData[posToIndex(x+1,y)] = RGBA{200,100,255,255};
+    }
+    if (x > 0) {
+        m_canvasData[posToIndex(x-1,y)] = RGBA{200,100,255,255};
+    }
+    if (y < 9) {
+            m_canvasData[posToIndex(x,y+1)] = RGBA{200,100,255,255};
+    }
+    if (y > 0) {
+        m_canvasData[posToIndex(x,y-1)] = RGBA{200,100,255,255};
+    }
 }
 
 void ColorCanvas::mouseDown(int x, int y) {
     // Task 10: update the variable that keeps track of whether the LMB is being
     //          held down, then color the pixel corresponding to the input (x, y)
     //          coordinate, if within canvas bounds
+    m_isDown = true;
 }
 
 void ColorCanvas::mouseMove(int x, int y) {
     // Task 9: color the pixel corresponding to the input (x, y) coordinate, if
     //         within canvas bounds
-
+//    if (x <= 9 & x >= 0 & y <= 9 & y >= 0 ) {
+//       m_canvasData[posToIndex(x,y)] = m_pencilColor;
+//    }
     // Task 10: update this function from the previous task so that the coloring
     //          of the canvas only happens if the LMB is being held down
+    if (x <= 9 & x >= 0 & y <= 9 & y >= 0 & m_isDown) {
+       m_canvasData[posToIndex(x,y)] = m_pencilColor;
+    }
 }
 
 void ColorCanvas::mouseUp(int x, int y) {
     // Task 10: update the variable that keeps track of whether the LMB is being
     //          held down
+    m_isDown = false;
 }
--- src/ui/graycanvas.cpp
+++ src/ui/graycanvas.cpp
@@ -1,21 +1,40 @@
 #include <cmath>
 #include "graycanvas.h"
+#include <iostream>
 
 uint8_t GrayCanvas::floatToInt(float intensity) {
     // Task 3: convert a float into a uint8_t
-
-    return 0;
+    return round(intensity * 255);
 }
 
 void GrayCanvas::initializeGrayCanvas() {
     // Task 4: initialize a 10 x 10 gray canvas where each pixel has an
     //         intensity of 0.123
+    std::vector<uint8_t> values(100);
+    uint8_t intensity = floatToInt(0.123);
+    std::fill(values.begin(), values.end(), intensity);
+    m_canvasData = values;
 
     // Task 5: call the createHeart() function to display the canvas with
     //         the white heart
+    createHeart();
 }
 
 void GrayCanvas::createHeart() {
     // Task 5: create a heart on the canvas by turning certain pixels white
     //         so it matches the canvas in the handout
+    m_canvasData[23] = floatToInt(1.0);
+    m_canvasData[24] = floatToInt(1.0);
+    m_canvasData[26] = floatToInt(1.0);
+    m_canvasData[27] = floatToInt(1.0);
+    m_canvasData[32] = floatToInt(1.0);
+    m_canvasData[35] = floatToInt(1.0);
+    m_canvasData[38] = floatToInt(1.0);
+    m_canvasData[42] = floatToInt(1.0);
+    m_canvasData[48] = floatToInt(1.0);
+    m_canvasData[53] = floatToInt(1.0);
+    m_canvasData[57] = floatToInt(1.0);
+    m_canvasData[64] = floatToInt(1.0);
+    m_canvasData[66] = floatToInt(1.0);
+    m_canvasData[75] = floatToInt(1.0);
 }
